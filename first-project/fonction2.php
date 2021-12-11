6cd19556d7092d3ff9dd58c32886b7149d930902
Samuel Lassoie <samuel.lassoie@gmail.com>
2021-12-11 11:26:21 +0100

    wip

--- first-project/fonction2.php
+++ first-project/fonction2.php
@@ -1,14 +1,49 @@
 <?php
-function addition(int $nb1, int $nb2) {
+function addition(int $nb1, int $nb2)
+{
     echo $nb1 + $nb2;
 }
 
-function addition2(int $nb1, int $nb2) {
+function addition2(int $nb1, int $nb2)
+{
     return $nb1 + $nb2;
 }
+function soustraction(int $nb1, int $nb2)
+{
+    return $nb1 - $nb2;
+}
+
+
+function calcul(int $nb1, int $nb2, string $operator)
+{
+    if ($operator == '+') {
+        return $nb1 + $nb2;
+    } else if ($operator == '-') {
+        return $nb1 - $nb2;
+    } else if ($operator == '/') {
+        return $nb1 / $nb2;
+    } else if ($operator == '*') {
+        return $nb1 * $nb2;
+    }
+}
+
+function calcul2(int $nb1, int $nb2, string $operator)
+{
+    echo eval("$nb1 $operator $nb2");
+    return 'yoo';
+}
+
+echo calcul2(100, 2, '*');
+exit;
+
+//fonction addition 
+// 3 paramètres => nb1, nb2, opérateur [+, -, /, *]
 
 echo '<h1>FONCTION</h1>';
 
+
+echo soustraction(10, 5);
+echo '<br>';
 
 // Additionner 4 et 10
 addition(4, 10);
@@ -31,5 +66,3 @@ echo '<br>';
 echo '<br>';
 $v2 = addition2(4, 10);
 var_dump($v2);
-
-
